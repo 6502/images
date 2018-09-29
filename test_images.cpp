74c663dca06b1efe5d5493a364d09b51cc7baba1
Andrea Griffini <agriff@tin.it>
2018-09-29 23:27:39 +0200

    removed argv dependency

--- test_images.cpp
+++ test_images.cpp
@@ -1,13 +1,10 @@
 #include "images.h"
-#include "argv.h"
 
 int main(int argc, const char *argv[]) {
-    PARM(std::string, src, "Source filename", "source.jpg");
-    PARM(std::string, dest, "Destination filename", "dest.bmp");
-
-    parse_argv("testimages", argc, argv);
-
-    saveImage(loadImage<unsigned>(src), dest);
-
+    if (argc != 3) {
+        printf("Syntax: test_images <source> <dest>\n");
+        return 1;
+    }
+    saveImage(loadImage<unsigned>(argv[1]), argv[2]);
     return 0;
 }
